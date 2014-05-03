4567cc69a70b72c9c85127a56de903758d0f7013
Fernandogza <fernandogarzaconde@gmail.com>
2014-05-03 03:25:33 -0500

    Update controlCuenta.java

---
+++ Controles/controlCuenta.java
@@ -0,0 +1,34 @@
+package controles;
+import entidades.cuenta;
+import java.util.Date;
+import java.util.Calendar;
+
+public class controlSuscripcion {
+   cuenta c;
+
+   public controlSuscripcion(){
+     c = new cuenta(); //Asume que la instancia persiste durante la sesion
+   }  
+   //Valida si la cuenta existe en la base de datos
+   public boolean validarCuenta(int ncuenta){            
+      return(c.validar(ncuenta));
+   }
+   public void crearCuenta(int ncuenta, String nombre, String correo, String telefono, String direccion){
+       c.crearCuenta(ncuenta, nombre, correo, telefono, direccion);   
+   }
+   public void actualizaNombre(int ncuenta, String nombre){
+       c.setNombre(ncuenta, nombre);
+   }
+   public void actualizaCorreo(int ncuenta, String correo){
+       c.setCorreo(ncuenta, correo);
+   }
+   public void actualizaTelefono(int ncuenta, String telefono){
+       c.setTelefono(ncuenta, telefono);
+   }
+   public void actualizaDireccion(int ncuenta, String direccion){
+       c.setDireccion(ncuenta, direccion);
+   }
+   public void cancelarCuenta(int ncuenta) {
+     //pendiente, depende de suscripciones tmb no se puede dejar una suscripcion abierta de una cuenta no existente.
+   }
+}
