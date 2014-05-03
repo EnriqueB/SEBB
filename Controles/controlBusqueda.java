0614dbbcdc14c700adbe0502d092c79fda370635
Enrique Bonilla <enriqueBonilla93@outlook.com>
2014-05-03 16:04:33 -0500

    Comentarios al control de busqueda

--- Controles/controlBusqueda.java
+++ Controles/controlBusqueda.java
@@ -1,23 +1,25 @@
 package controles;
 import entidades.cuenta;
 import entidades.articulo;
-import entidades.suscripcion;
 
 public class controlBusqueda {
    cuenta c;
    articulo a;
-   suscripcion s;
    
    public controlBusqueda(){
      c = new cuenta(); //Asume que la instancia persiste durante la sesion
      a = new articulo();
    }
    public boolean validarBusqueda(int IDC, int IDA){
+       //este metodo revisa si el usuario puede acceder al articulo
+       //dependiendo del tipo de cuenta que tenga
        String t = c.getTipo(IDC);
-       boolean p = a.getPublicado(IDA);
+       //las cuentas de juez, admin y editor pueden acceder a cualquier articulo que exista
        if(t.equals("Juez")||t.equals("Admin")||t.equals("Editor")){
            return true;
        }
+       boolean p = a.getPublicado(IDA);
+       //si es un usuario normal entonces solo puede verlo si esta publicado
        return p;
    }
    public String busqueda(int IDA){
