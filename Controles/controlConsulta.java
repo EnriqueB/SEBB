c6a51ac9cb97f515198c6d257231eb623b9d3e4a
Fernandogza <fernandogarzaconde@gmail.com>
2014-05-03 03:10:57 -0500

    Update controlConsulta.java

---
+++ Controles/controlConsulta.java
@@ -0,0 +1,39 @@
+package controles;
+import entidades.cuenta;
+import entidades.suscripcion;
+import java.util.Date;
+import java.util.Calendar;
+
+public class controlSuscripcion {
+   cuenta c;
+   suscripcion s;
+
+   public controlSuscripcion(){
+     c = new cuenta(); //Asume que la instancia persiste durante la sesion
+     s = new suscripcion();
+   }  
+   public String consultaTipo(int IDC){
+       return(s.getTipo(s.getSuscripcion(IDC)));
+   }
+   public int consultaDuracion(int IDC){
+       return(s.getDuracion(s.getSuscripcion(IDC)));
+   }
+   public Calendar consultaInicio(int IDC){
+       return (s.getInicio(s.getSuscripcion(IDC)));
+   }
+   public Calendar consultaFin(int IDC){
+       return (s.getFin(s.getSuscripcion(IDC)));
+   }
+   public String consultaNombre(int ncuenta) {
+     return (c.getNombre(ncuenta));
+   }
+   public String consultaCorreo(int ncuenta) {
+     return (c.getCorreo(ncuenta));
+   }
+   public String consultaTelefono(int ncuenta) {
+     return (c.getTelefono(ncuenta));
+   }
+   public String consultaDireccion(int ncuenta) {
+     return (c.getDireccion(ncuenta));
+   }
+}
