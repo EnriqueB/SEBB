4a0fe78416da38d44e6c120643a994c8e00ee958
Jaguz <jadavax@hotmail.com>
2014-05-03 22:16:31 -0500

    control publicacion
    contro publicacion hecho,

--- Entidades/autor.java
+++ Entidades/autor.java
@@ -5,14 +5,5 @@ class autor extends cuenta{
     public void revisarArticuloPublicado(){
         //pendiente
     }
-    public void crearArticulo(int ID, String autor, int longitud, String titulo){
-        try{
-            String s = "INSERT INTO Articulo (IDArticulo, Autor, Longitud, Titulo, Votos, Tipo)" + " VALUES ("+ID+" , '"+ autor + "' , " + longitud + " , 0 , '" + titulo + "' , 'No publicado')";
-            System.out.println(s); 
-            stmt.executeUpdate(s);
-        }
-        catch(Exception e){
-            System.out.println ("Cannot update database" + e );
-        }
-    }
+
 }
