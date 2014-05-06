f08d2df11199be0aaa4248b77a6126a825e2e010
Fernandogza <fernandogarzaconde@gmail.com>
2014-05-06 08:22:33 -0500

    Update Cuenta.java

--- Entidades/Cuenta.java
+++ Entidades/Cuenta.java
@@ -8,7 +8,7 @@ public class Cuenta{
     public Cuenta(){
         try{
             String userName="root";
-            String password="admin";
+            String password="password";
             String url = "jdbc:mysql://localhost/SEBB";
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             conn = DriverManager.getConnection (url, userName, password);
