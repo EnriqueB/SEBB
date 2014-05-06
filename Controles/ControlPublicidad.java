3692967c57aa1b7b3b47d5d5570b478e688e3d2c
Fernandogza <fernandogarzaconde@gmail.com>
2014-05-05 21:40:48 -0500

    Update ControlPublicidad.java

--- Controles/ControlPublicidad.java
+++ Controles/ControlPublicidad.java
@@ -1,14 +1,16 @@
 package controles;
 import entidades.Edicion;
+import entidades.Cuenta;
 import entidades.Anuncios;
 
 public class ControlPublicidad {
    Edicion e;
    Anuncios a;
-   
+   Cuenta c;
    public ControlPublicidad(){
      e = new Edicion(); //Asume que la instancia persiste durante la sesion
      a = new Anuncios();
+     c= new Cuenta();
    }
    public boolean validarEdicion(int ID){
        return e.validarEdicion(ID);
@@ -31,5 +33,12 @@ public class ControlPublicidad {
    }
    public int [] getListaAnuncios(int IDE){
        return a.getAnuncios(IDE);
+   }
+    public boolean validarCuentaAdminEditor(int IDC){
+       String t = c.getTipo(IDC);
+       if(t.equals("Admin") || t.equals("Editor")){
+           return true;
+       }
+       return false;
    }
 }
