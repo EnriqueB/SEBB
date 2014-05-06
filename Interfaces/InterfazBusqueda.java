38a51cd577bffd8d5460bfdb782edf070879f165
Fernandogza <fernandogarzaconde@gmail.com>
2014-05-06 01:39:26 -0500

    Update InterfazBusqueda.java

--- Interfaces/InterfazBusqueda.java
+++ Interfaces/InterfazBusqueda.java
@@ -1,20 +1,21 @@
 package interfaces;
-import controles.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 import java.util.*;
 import controles.ControlCuenta;
-import controles.ControlPublicacion;
+import controles.ControlBusqueda;
 import controles.ControlInicioSesion;
+import controles.ControlSuscripcion;
 
 public class InterfazBusqueda extends HttpServlet {
   HttpServletResponse thisResponse;
   HttpServletRequest thisRequest;
   PrintWriter out;
   ControlInicioSesion ci;
-  ControlPublicacion cp;
+  ControlBusqueda cb;
   ControlCuenta cu;
+  ControlSuscripcion cs;
   //Es importante observar que todos los metodos definen la operacion GET para
   //que el metodo doGet sea el que se ejecuta al presionar el boton "Enviar". 
   public void doGet(HttpServletRequest request,
@@ -36,37 +37,80 @@ public class InterfazBusqueda extends HttpServlet {
     String operacion = request.getParameter("operacion");
     if(operacion == null){ // El menu nos envia un parametro para indicar el inicio de una transaccion
       buscar();  
-    }else if(operacion.equals("agregar")){
-       //agregado();
+    }else if(operacion.equals("resultado")){
+       busqueda();
+    }else if(operacion.equals("desplegar")) {
+    	desplegar();
     }
   }
   
-     public int [] buscar(){
-	String busqueda="";
-	out.println("<input type=\"text\" name=\"busqueda\" size=\"15\"></p>");
-	int [] articulos = new int [1];
-        articulos[0]=-1;
-        int count;
-        try {
-            stmt.executeQuery("SELECT COUNT(IDArticulo) as cant FROM Articulo WHERE Votos > 3 AND Publicado = 0 AND Titulo IN busqueda");
-            ResultSet rs = stmt.getResultSet();
-            rs.next();
-            count=rs.getInt("cant");
-            rs.close();
-            stmt.executeQuery("SELECT IDArticulo FROM Articulo WHERE Votos > 3 AND Publicado = 0 AND Titulo IN busqueda");
-            rs = stmt.getResultSet();
-            rs.next();
-            int [] articulosEncontrados = new int [count];
-            for(int i=0; i<count; i++){
-                articulosEncontrados[i]=rs.getInt("IDArticulo");
-                rs.next();
-            }
-            rs.close();
-            return(articulosEncontrados);
-        }
-        catch(SQLException e){
-            System.out.println("Cannot getArticulos()"+e);
-        }
-        return articulos;
-  } 
+	public void buscar(){
+	        out.println("<p>Bienvenido, Usuario</p>");
+	        out.println("<p>Escriba el titulo o frase que identifique el articulo que quiere buscar.</p>");
+	        out.println("<form method=\"GET\" action=\"Buscar Articulos\">");
+	        out.println("<input type=\"hidden\" name=\"operacion\" value=\"resultado\"/>");
+	        out.println("<input type=\"text\" name=\"texto\" size=\"15\"></p>");
+	        out.println("<p><input type=\"submit\" value=\"Enviar\"name=\"B1\"></p>");
+	        out.println("</form>");
+	
+	        out.println("<form method=\"GET\" action=\"menu.html\">");
+	        out.println("<p><input type=\"submit\" value=\"Cancelar\"name=\"B2\"></p>");
+	        out.println("</form>");
+	
+	        out.println("</BODY>");
+	        out.println("</HTML>"); 
+  	}
+  	public void busqueda() {
+        	cb = new ControlBusqueda();
+        	String texto = thisRequest.getParameter("texto").trim();
+        	String art [] = cb.busqueda(texto);
+        	if(art.length()>0) {
+		        out.println("<p>Resultados de la busqueda</p>");
+			for(int i=0; i<art.length; i++) {
+	            		out.println("<p>"+art[i]+"</p>");
+			}
+			out.println("<p>Escriba el titulo exacto del articulo que quiera abrir.</p>");
+		        out.println("<form method=\"GET\" action=\"Buscar Articulos\">");
+		        out.println("<input type=\"hidden\" name=\"operacion\" value=\"desplegar\"/>");
+		        out.println("<input type=\"text\" name=\"articulo\" size=\"15\"></p>");
+		        out.println("<p><input type=\"submit\" value=\"Enviar\"name=\"B1\"></p>");
+		        out.println("</form>");
+		
+		        out.println("<form method=\"GET\" action=\"menu.html\">");
+		        out.println("<p><input type=\"submit\" value=\"Cancelar\"name=\"B2\"></p>");
+		        out.println("</form>");
+		
+		        out.println("</BODY>");
+		        out.println("</HTML>"); 
+        	} else {
+        		out.println("<p>No se encontraron articulos bajo ese nombre.</p>");
+   			out.println("<form method=\"GET\" action=\"menu.html\">");
+		        out.println("<p><input type=\"submit\" value=\"Cancelar\"name=\"B2\"></p>");
+		        out.println("</form>");
+		        out.println("</BODY>");
+		        out.println("</HTML>"); 
+        	}
+  	}
+  	public void desplegar() {
+  		String articulo = thisRequest.getParameter("articulo").trim();
+  		ci = new ControlInicioSesion();
+	        String n = ci.getConected();
+	        cu = new ControlCuenta();
+	        cb = new ControlBusqueda();
+	        cs = new ControlSuscripcion();
+	        int userID = cu.obtenerID(n);
+	        if(cb.validarBusqueda(userID) || cs.validarSus(userID)) {
+	        	String textoC = cb.getArticulo(articulo);
+	        }
+	        else {
+	        	String textoC = cb.getArticuloRestringido(articulo);
+	        }
+	        out.println("<p>" + articulo + "<p>");
+	        out.println("<p>" + textoC + "<p>");
+   		out.println("<form method=\"GET\" action=\"menu.html\">");
+	        out.println("<p><input type=\"submit\" value=\"Cancelar\"name=\"B2\"></p>");
+	        out.println("</form>");
+	        out.println("</BODY>");
+	        out.println("</HTML>"); 
+  	}
 }
