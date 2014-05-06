f76f1eabbcfc3e7defbaa7f69ae91c70522566ba
Fernandogza <fernandogarzaconde@gmail.com>
2014-05-06 01:49:06 -0500

    Update Anuncios.java

--- Entidades/Anuncios.java
+++ Entidades/Anuncios.java
@@ -10,7 +10,7 @@ public class Anuncios {
     public Anuncios(){
         try{
             String userName="root";
-            String password="password";
+            String password="admin";
             String url = "jdbc:mysql://localhost/SEBB";
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             conn = DriverManager.getConnection (url, userName, password);
